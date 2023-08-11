e656c7c1c59bc8beaa0120eb30ca75f862ccf30a
mayowa akinrinlola <mayowa.akinrinlola@yahoo.com>
2023-08-11 21:47:26 +0100

    python programming

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
+    i = len(sys.argv) - 1
 
-argv = sys.argv[1:]
-num_args = len(argv)
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
 
-if num_args == 0:
-    print("0 arguments.")
-elif num_args == 1:
-    print("1 argument:")
-    print("1: {}".format(argv[0]))
-else:
-    print("{} arguments:".format(num_args))
-    for i, arg in enumerate(argv, start=1):
-        print("{}: {}".format(i, arg))
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
