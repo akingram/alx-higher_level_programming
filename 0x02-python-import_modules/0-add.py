4b0e21340bf74b8e50b0b3bbcd59946994490df9
mayowa akinrinlola <mayowa.akinrinlola@yahoo.com>
2023-08-11 13:42:47 +0100

    python programming

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Define the add function
+def add(a, b):
+    return a + b
+
+# Assign values to variables
+a = 1
+b = 2
+
+# Format and print the values
+print("{} + {} = {}".format(a, b, add(a, b)))
